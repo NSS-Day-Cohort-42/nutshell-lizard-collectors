e49d5351a24940ebe758f5f56a61796f2dcea6c4
cwelshofer <cwelshofer@gmail.com>
2020-08-27 16:01:09 -0500

    new code

--- src/scripts/tasks/taskHTMLConverter.js
+++ src/scripts/tasks/taskHTMLConverter.js
@@ -11,15 +11,18 @@ export const TaskEntryComponent = (tasks) => {
 
            <button id="entryDelete--${ tasks.id }">Delete</button>
          <button id="editEntry--${tasks.id}">Edit</button>
-         <button id="tasksRecordButton--${tasks.id}">Mark as complete</button>
+         <button id="taskRecordButton--${tasks.id}">Mark as complete</button>
 
-         <button id="tasksRecordButton">Record Tasks</button>
+         <button id="taskRecordButton">Record Tasks</button>
         
          <fieldset class="TaskEntry">
          <label id=taskLabel for="taskEntry">Journal Entry</label>
          <textarea id="taskRecord" name="taskEntry" rows="4" cols="50"
            style="margin: 0px 347px 0px 0px; width: 128px; height: 52px;"></textarea>
        </fieldset>
+       <input type="hidden" name="entryId" id="entryId" value="">
+       <input type="hidden" name="taskUserId" id="taskUserId" value="">
+        <input type="hidden" name="taskCompleted" id="taskCompleted" value="">
 
          
         </section>
@@ -27,17 +30,22 @@ export const TaskEntryComponent = (tasks) => {
 }
 
 eventHub.addEventListener("click", clickEvent => {
+    
     console.log("event")
     if(clickEvent.target.id === "taskRecordButton") {
+        debugger
         const taskId = document.querySelector("#entryId");
         const taskEntry = document.querySelector("#taskRecord")
-        const taskUserId = document.querySelector()
+        const taskUserId = document.querySelector("#taskUserId")
+        const taskCompleted = document.querySelector("#taskCompleted")
        
         
         const newTask = {
             id: taskId.value,
-            userId:
+            userId: taskUserId.value,
             entry: taskEntry.value,
+            complete: taskCompleted.value
+
 
 
             
