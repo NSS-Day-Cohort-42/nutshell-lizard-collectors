5b020adbae1c6f97f1ee1735d162aa330d830ba9
FORDBA <bfordcpa@gmail.com>
2020-08-27 16:03:53 -0500

    can now save events

--- src/scripts/events/eventHTMLConverter.js
+++ src/scripts/events/eventHTMLConverter.js
@@ -1,3 +1,5 @@
+import { saveEvent } from "./eventDataProvider.js"
+
 const eventHub = document.querySelector(".container")
 
 
@@ -23,18 +25,31 @@ return`
         <div>Event Name:<input type="text" id="eventForm--name" placeholder="Enter Event Name" /></div>
         <div>City: <input type="text" id="eventForm--city" placeholder="eg. Nashville" /></div>
         <div>State:<input type="text" id="eventForm--state" placeholder="TN" maxlength="2" />
-        <div>Date:<input type="date" name="eventDate" id="event--date"></div>
+        <div>Date:<input type="date" name="eventDate" id="eventForm--date"></div>
         <button id="closeEvent">Close</button>
         <button id="saveEvent">Create Event</button>
         
         `
     }
 
-    // eventHub.addEventListener('click', event => {
-    //     if (event.target.id === 'closeEvent') {
-    //         const dialog = event.target.parentNode
-    //         dialog.close()
-    //     }
-    // })
+ 
     
-    
+    eventHub.addEventListener("click", clickEvent => {
+        if (clickEvent.target.id === "saveEvent") {
+            const eventName = document.querySelector("#eventForm--name")
+            const eventCity = document.querySelector("#eventForm--city")
+            const eventState = document.querySelector("#eventForm--state")
+            const eventDate = document.querySelector("#eventForm--date")
+           
+                
+            const newEvent = {
+                name: eventName.value,
+                city: eventCity.value,
+                state: eventState.value,
+                date: eventDate.value
+
+
+            }
+            saveEvent(newEvent)
+        }
+    })
