8e79b83874d18b50ab25f1b05106ec3ec09e2dbf
Chris Johnson <christopherjohnson1@gmail.com>
2020-08-27 15:03:20 -0400

    modal works

--- src/scripts/news/newsHTMLConverter.js
+++ src/scripts/news/newsHTMLConverter.js
@@ -2,12 +2,30 @@ export const newsHTML = (newsObj) => {
     return `
     <div class="news">
     <h3>News</h3>
-    <button>New Article</button>
-    <p>
-        title: ${ newsObj.title }
-        synopsis: ${ newsObj.synopsis }
-        link: ${ newsObj.url }
-    </p>
+    <button id="new-article-btn">New Article</button>
+        <p>Title: ${ newsObj.title }</p>
+        <p>Synopsis: ${ newsObj.synopsis }</p>
+        <p>Link: ${ newsObj.url }</p>
     </div>
+    `
+}
+
+export const newsFormModal = () => {
+    return `
+    <form>
+    <h1>Save New Article</h1>
+    <fieldset>
+    <label for="news-title">Article Title:</label>
+    <input type="text" id="news-title" name="news-title" placeholder="Input article name here">
+    </fieldset>
+    <fieldset>
+    <label for="news-synopsis">Synopsis</label>
+    <textarea class="newsSynopsis" id="news-synopsis" placeholder="The highlights of this article are..."></textarea>
+    </fieldset>
+    <fieldset>
+    <label for="news-URL">Article URL</label>
+    <input type="text" id="news-URL" name="news-URL" placeholder="Input article URL here">
+    </fieldset>
+    </form>
     `
 }
--- src/scripts/news/newsList.js
+++ src/scripts/news/newsList.js
@@ -1,13 +1,29 @@
 import { getNews, useNews } from "./newsDataProvider.js"
-import { newsHTML } from "./newsHTMLConverter.js"
+import { newsHTML, newsFormModal } from "./newsHTMLConverter.js"
 
 const contentTarget = document.querySelector(".contentLeft--main__rightNews")
+const eventHub = document.querySelector(".container")
+
+eventHub.addEventListener('click', (clickEvent) => {
+    if (clickEvent.target.id === "new-article-btn") {
+        // const dialog = newsFormModal()
+        newArticleFormRender()
+        const dialog = document.querySelector(".modalContainer--news")
+        dialog.showModal()
+    }
+})
 
 const render = () => {
     const newsRender = useNews()
     contentTarget.innerHTML = `
     ${newsRender.map(newsObj => newsHTML(newsObj)).join('')}
+    <dialog class="modalContainer--news"></dialog>
     `
+}
+
+const newArticleFormRender = () => {
+    const modalTarget = document.querySelector(".modalContainer--news")
+    modalTarget.innerHTML = newsFormModal()
 }
 
 export const newsList = () => {
